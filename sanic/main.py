4b59d25155da3f44c15541019f66577ba3d5dee9
yemq3 <ymqgmaail@gmail.com>
2020-09-27 18:25:15 +0800

    add sanic

--- sanic/main.py
+++ sanic/main.py
@@ -1,5 +1,11 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic.websocket import WebSocketProtocol
+from app.yolov4 import *
+import json
+import cv2
+import pickle
+import asyncio
 
 app = Sanic("test")
 
@@ -7,5 +13,35 @@ app = Sanic("test")
 async def test(request):
     return json({"hello": "world"})
 
+@app.websocket('/sanic')
+async def getImage(request, ws):
+    while True:
+        data = await ws.recv()
+        # print(time.time())
+        recv_data = pickle.loads(data)
+
+        nparr = np.frombuffer(recv_data["image"], np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        sized = cv2.resize(img, (darknet.width, darknet.height))
+        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+        
+        boxes = do_detect(darknet, sized, 0.4, 0.6, USE_CUDA)
+        boxes = np.array(boxes[0]).tolist()
+
+        for i in range(len(boxes)):
+            boxes[i][6] = class_names[int(boxes[i][6])]
+        
+        response = {
+            'frameid': recv_data["frameid"],
+            'boxes' : boxes
+        }
+
+        response_data = pickle.dumps(response)
+
+        await ws.send(response_data)
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(protocol=WebSocketProtocol)
